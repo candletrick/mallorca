893e5d407b24f3ba0ead2b5461e0830180e09d88
Blake <yatsuha@fastmail.se>
2015-08-24 17:05:47 -0700

    Email in reply to.

--- lib/Email/Email.php
+++ lib/Email/Email.php
@@ -2,23 +2,29 @@
 
 class Email
 	{
-	static public function send($to, $subject, $msg, $html = true, $fromname = 'Socrates', $from = 'socrates@emailsock.com')
+	static public function send($to, $subject, $msg, $html = true, $fromname = 'Socrates', $from = 'socrates@emailsock.com', $headers = array())
 		{
-		$headers = "From: \"$fromname\" <$from>"
+		$hs = "From: \"$fromname\" <$from>"
 		. "\r\nReply-To: $from"
 		. "\r\nX-Mailer: PHP/' . phpversion()"
 		;
 
 		// To send HTML mail, the Content-type header must be set
 		if ($html) {
-			$headers .= "\r\nMIME-Version: 1.0"
+			$hs .= "\r\nMIME-Version: 1.0"
 			. "\r\nContent-type: text/html; charset=iso-8859-1"
-			. "\r\n"
 			;
 			}
 
+		$new_message_id = md5(time() . rand()) . $_SERVER['HTTP_HOST'];
+		$hs .= "\r\nMessage-ID: $new_message_id";
+
+		foreach ($headers as $k=>$v) $hs .= "\r\n$k: $v";
+
+		$hs .= "\r\n";
+
 		// testing
 		// mail('fewkeep@gmail.com', $subject . " on behalf of " . $to, $msg, $headers);
-		return mail($to, $subject, $msg, $headers);
+		return mail($to, $subject, $msg, $hs);
 		}
 	}
