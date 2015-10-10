00bb0378abc59d48e169f3328f6464c4acf3908b
Blake <yatsuha@fastmail.se>
2015-10-10 14:21:16 -0700

    Textarea counter

--- lib/Input/Textarea.php
+++ lib/Input/Textarea.php
@@ -3,9 +3,22 @@ namespace Input;
 
 class Textarea extends \Input
 	{
+	public $len;
+
+	public function my_construct($len = 250)
+		{
+		$this->len = $len;
+		}
+
 	public function my_input()
 		{
-		return "<textarea class='$this->classes' name='$this->name' rows='4' cols='44'>"
-		. $this->esc_value() . "</textarea>";
+		// return "<textarea class='$this->classes' name='$this->name' rows='4' cols='44'>"
+		// . $this->esc_value() . "</textarea>";
+		return "<div class='tbox'><div class='counter' id='" . $this->name . "_count'>$this->len</div>"
+		. "<textarea name='$this->name' id='$this->name'"
+		// . " rows='$this->rows' cols='$this->columns'
+		. " onfocus=\"return char_counter(event, this, $this->len);\"
+		onkeydown=\"return char_counter(event, this,$this->len);\"
+		onpaste=\"return char_counter(event, this,$this->len);\">$this->value</textarea><div class='rug'></div></div>";
 		}
 	}
